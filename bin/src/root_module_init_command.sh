10c0b54320ef0144e7ac27ddf6cdc9ee58e1bd4c
wevans <paul@wevans.io>
2023-03-22 14:18:55 -0400

    Allow conditionally syncing providers

--- bin/src/root_module_init_command.sh
+++ bin/src/root_module_init_command.sh
@@ -2,8 +2,14 @@ parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 template=${parent_path}/../terraform/templates/root-module
 dest=${parent_path}/../terraform/root-modules/${args[name]}
 
-echo "Creating a new terraform root module at ${dest} from template"
+echo "Creating a new terraform root module: ${args[name]}"
 
 cp -a ${template} ${dest}
 sed -i "" "s/<module_name>/${args[name]}/g" $dest/backend.tf
+
+for provider in $(echo ${args[--providers]} | sed "s/,/ /g"); do
+  echo "Using provider ${provider} in new module"
+  cd $dest
+  ln -s ../../providers/${provider}.tf ${provider}.tf
+done
 
